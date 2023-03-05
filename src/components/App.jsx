8cd7ac3b797910727bd2685c75563872e7ad8ad0
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-05 10:47:43 +0600

    router added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
 import { CssBaseline } from '@mui/material';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Movies from './Movies/Movies';
+import NavBar from './NavBar/NavBar';
+import Profile from './Profile/Profile';
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <CssBaseline />
-      <h1>hello world</h1>
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />}></Route>
+          <Route path="Profile" element={<Profile />}></Route>
+          <Route path="Profile/:id" element={<Profile />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 };
